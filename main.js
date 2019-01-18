44fc3c8cf290d561c27e1d811e4ae7082ffa416c
Marcin <marcinrwisniowski@gmail.com>
2019-01-18 03:16:37 -0500

    Progress bars and 3D Perspective

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const startBtn = document.querySelector("#start-timer");
 const resetBtn = document.querySelector("#reset-timer");
 const inputminutes = document.querySelector("#minutestimer input");
 const inputseconds = document.querySelector("#secondstimer input");
+const secProgress = document.querySelector("#second-progress");
+const minProgress = document.querySelector("#minute-progress");
 var running = false;
 var seconds;
+var startseconds;
 var timeElapsed = 0;
 
 
@@ -21,13 +24,15 @@ inputseconds.addEventListener('keyup',function(e){
 function startClick(){
     //Insert value of seconds into variable for future math purposes and check that no other instance is running.
     seconds = Number(inputminutes.value)*60 + Number(inputseconds.value);
+    startseconds = seconds;
     if (!running && seconds > 0){
+        running = true;
         countDown(seconds);
         startBtn.style.backgroundColor = "#C32026";
         startBtn.style.border = "none";
         startBtn.style.color = "#FFF";
          }
-    running = true;
+    
 }
 
 function countDown(seconds){
@@ -63,6 +68,8 @@ function endClick(){
         running = false;
         inputminutes.value="";
         inputseconds.value="";
+        minProgress.style.height = "0%";
+        secProgress.style.height = "0%";
         startBtn.style.backgroundColor = "#FFF";
         startBtn.style.border = "5px solid #C32026";
         startBtn.style.color = "#000";
@@ -71,5 +78,11 @@ function endClick(){
 function setHTML(seconds){
         inputminutes.value = Math.floor(seconds/60);
         inputseconds.value = seconds - Math.floor(seconds/60)*60;
+
+        console.log((seconds / startseconds * 100));
+        console.log(inputseconds.value / 60 * 100);
+
+        minProgress.style.height = (seconds / startseconds * 100) + "%";
+        secProgress.style.height = (inputseconds.value / 60 * 100) + "%";
 }
 
